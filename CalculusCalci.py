0c26f1c2b9e9be61b0efd0e3fb0fdc8cb0e59dcf
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2021-07-29 19:31:29 +0530

    Update CalculusCalci.py

--- CalculusCalci.py
+++ CalculusCalci.py
@@ -1,10 +1,9 @@
-#Imports:
 import streamlit as st
 import latex
-from latex2sympy2 import latex2sympy, latex2latex
+from latex2sympy2 import latex2sympy
 from sympy import *
 st.title("Calculus Calci")
-st.sidebar.write("Enter your expression, and this will give you the first two derivatives, the indefinite integral (with respect to x), and the first three terms of its Maclaurin series.\n\n -    Saumya Shah")
+st.sidebar.write("Enter your expression, and this will give you the first two derivatives, the indefinite integral (with respect to x), and the fifth order of its Maclaurin series.\n\n -    Saumya Shah")
 st.write("Enter f(x):")
 rawInp = st.text_input("Please enter your function in latex and press enter (do not use 'c')")
 if (rawInp):
@@ -18,5 +17,6 @@ if (rawInp):
     st.latex("f''(x) = " + str(latex(simplify(diff(diff(symInp,x),x)))))
     st.write("Indefinite Integral:")
     st.latex("\int f(x) dx = " + str(latex(simplify(integrate(symInp,x)))) + " + C")
-    st.write("Maclaurin Series:")
-    st.latex(str(latex(simplify(symInp.subs(x,0)) + simplify(x*(diff(symInp,x)).subs(x,0))) + simplify(x**2*(diff(diff(symInp,x),x)).subs(x,0)/2)))
+    st.write("5th order Maclaurin Series:")
+    MaclaurinSeries = (symInp.subs(x,0)) + (x*(diff(symInp,x)).subs(x,0)) + (x**2*(diff(diff(symInp,x),x)).subs(x,0)/2) + (x**3*(diff(diff(diff(symInp,x),x),x)).subs(x,0)/6) + (x**4*(diff(diff(diff(diff(symInp),x),x),x)).subs(x,0)/24) + (x**5*(diff(diff(diff(diff(diff(symInp,x)),x),x),x)).subs(x,0)/120)
+    st.latex(str(latex(simplify(MaclaurinSeries))))
