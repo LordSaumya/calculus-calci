7aaf86bdab4a1f6723c1d7c680335b8e8aff14a4
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2021-07-29 11:54:42 +0530

    Update CalculusCalci.py

--- CalculusCalci.py
+++ CalculusCalci.py
@@ -4,7 +4,7 @@ import latex
 from latex2sympy2 import latex2sympy, latex2latex
 from sympy import *
 st.title("Calculus Calci")
-st.sidebar.write("Enter your expression, and this will give you the first two derivatives and the indefinite integral (with respect to x).\n\n -    Saumya Shah")
+st.sidebar.write("Enter your expression, and this will give you the first two derivatives, the indefinite integral (with respect to x), and the first three terms of its Maclaurin series.\n\n -    Saumya Shah")
 st.write("Enter f(x):")
 rawInp = st.text_input("Please enter your function in latex and press enter (do not use 'c')")
 if (rawInp):
@@ -18,4 +18,5 @@ if (rawInp):
     st.latex("f''(x) = " + str(latex(simplify(diff(diff(symInp,x),x)))))
     st.write("Indefinite Integral:")
     st.latex("\int f(x) dx = " + str(latex(simplify(integrate(symInp,x)))) + " + C")
-
+    st.write("Maclaurin Series:")
+    st.latex(str(latex(simplify(symInp.subs(x,0)) + simplify(x*(diff(symInp,x)).subs(x,0))) + simplify(x**2*(diff(diff(symInp,x),x)).subs(x,0)/2)))
