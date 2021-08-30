37a7da131f1d3362a38235e7da3cc892b87ca31c
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2021-08-30 14:29:57 +0530

    Update CalculusCalci.py

--- CalculusCalci.py
+++ CalculusCalci.py
@@ -4,22 +4,24 @@ from humanize import ordinal
 from latex2sympy2 import latex2sympy
 from sympy import *
 
-def mac(func,ord):
+def tay(func,ord,cc):
     x = symbols("x")
-    MacS = func.subs(x,0)
-    for i in range(1,ord+1):
-        MacS += simplify(((x**i*(diff(func,x,i)).subs(x,0))/factorial(i)))
-    return MacS
+    tayS = 0
+    for i in range(0,ord+1):
+        tayS += simplify((((x-cc)**i*(diff(func,x,i)).subs(x,cc))/factorial(i)))
+    return tayS
 
 st.title("Calculus Calci")
 st.sidebar.write("Enter your expression, and this will give you the first two derivatives, the indefinite integral (with respect to x), and the fifth order of its Maclaurin series.\n\n -    Saumya Shah")
 st.write("Enter f(x):")
 rawInp = st.text_input("Please enter your function in latex and press enter (do not use 'c')")
-st.write("Enter order of Maclaurin Polynomial:")
-mOrder = st.number_input("Please enter a positive integer for the order of Maclaurin polynomial")
-if (rawInp and mOrder):
-    mOrder = abs(round(mOrder))
-    st.warning("Large values for the order of Maclaurin polynomial may take more time to process.")
+st.write("Enter order of Taylor Polynomial:")
+tOrder = st.number_input("Please enter a positive integer for the order of Taylor series")
+st.write("Enter centre of convergence for Taylor series")
+cc = st.number_input("Enter a x-value for the centre of convergence for Taylor series (write 0 for Maclaurin polynomials)")
+if (rawInp and tOrder):
+    tOrder = abs(round(tOrder))
+    st.warning("Large values for the order of Taylor series may take more time to process.")
     x = symbols("x")
     symInp = latex2sympy(rawInp)
     st.write("Simplified Input:")
@@ -30,5 +32,5 @@ if (rawInp and mOrder):
     st.latex("f''(x) = " + str(latex(simplify(diff(diff(symInp,x),x)))))
     st.write("Indefinite Integral:")
     st.latex("\int f(x) dx = " + str(latex(simplify(integrate(symInp,x)))) + " + C")
-    st.write(ordinal(mOrder) + " order Maclaurin Series:")
-    st.latex(str(latex(simplify(mac(symInp,mOrder)))))
+    st.write(ordinal(tOrder) + " order Taylor Series with centre of convergence = " + str(cc) + ":")
+    st.latex(str(latex(simplify(tay(symInp,tOrder,cc)))))
